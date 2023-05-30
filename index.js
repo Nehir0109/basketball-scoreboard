2424f4eb1c1bcb58e0e182ef6adb204896d9396a
Nehir Çakılcı <nehir.cakilci@gmail.com>
2023-05-30 15:52:29 +0300

    javaScript

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const homeScoreHolder = document.getElementById("homeScore");
+const guestScoreHolder = document.getElementById("guestScore");
 
+let homeScore = 0;
+let guestScore = 0;
+
+
+function incrementHomeOne(){
+    homeScore += 1;
+    homeScoreHolder.innerHTML = homeScore;
+}
+function incrementHomeTwo(){
+    homeScore += 2;
+    homeScoreHolder.innerHTML = homeScore;
+}
+function incrementHomeThree(){
+    homeScore += 3;
+    homeScoreHolder.innerHTML = homeScore;
+}
+
+
+function incrementGuestOne(){
+    guestScore += 1;
+    guestScoreHolder.innerHTML= guestScore;
+}
+function incrementGuestTwo(){
+    guestScore +=2;
+    guestScoreHolder.innerHTML= guestScore;
+}
+function incrementGuestThree(){
+    guestScore +=3;
+    guestScoreHolder.innerHTML= guestScore;
+}
