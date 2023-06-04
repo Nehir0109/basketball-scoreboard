c1b8e27e51e98baade04c95cc1ae82f0328304b1
Nehir Çakılcı <nehir.cakilci@gmail.com>
2023-06-04 16:37:06 +0300

    correction

--- index.js
+++ index.js
@@ -5,29 +5,13 @@ let homeScore = 0;
 let guestScore = 0;
 
 
-function incrementHomeOne(){
-    homeScore += 1;
-    homeScoreHolder.innerHTML = homeScore;
-}
-function incrementHomeTwo(){
-    homeScore += 2;
-    homeScoreHolder.innerHTML = homeScore;
-}
-function incrementHomeThree(){
-    homeScore += 3;
+function incrementHome(score){
+    homeScore += score;
     homeScoreHolder.innerHTML = homeScore;
 }
 
 
-function incrementGuestOne(){
-    guestScore += 1;
-    guestScoreHolder.innerHTML= guestScore;
-}
-function incrementGuestTwo(){
-    guestScore +=2;
-    guestScoreHolder.innerHTML= guestScore;
-}
-function incrementGuestThree(){
-    guestScore +=3;
+function incrementGuest(score){
+    guestScore += score;
     guestScoreHolder.innerHTML= guestScore;
 }
